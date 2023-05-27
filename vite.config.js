ae12607768f1f74b165ba304c800404f3c0b3e0f
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-05-27 22:00:16 +0300

    change vite.confige

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/food-delyvery-front/",
+  server: {
+    routes: [
+      { path: "/", component: "/src/pages/Home.jsx" },
+      { path: "/drinks", component: "/src/pages/Drinks.jsx" },
+      { path: "/cart", component: "/src/pages/Cart.jsx" },
+      { path: "/history", component: "/src/pages/History.jsx" },
+    ],
+  },
 });
