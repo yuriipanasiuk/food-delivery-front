8f0ee220a737ee35501431fde2ef7ab3a47837b4
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2023-05-27 22:45:26 +0300

    change basename in main.js

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="food-delyvery-front">
+    <BrowserRouter basename="/food-delyvery-front">
       <Provider store={store}>
         <App />
       </Provider>
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/food-delyvery-front/",
-  server: {
-    routes: [
-      { path: "/", component: "/src/pages/Home.jsx" },
-      { path: "/drinks", component: "/src/pages/Drinks.jsx" },
-      { path: "/cart", component: "/src/pages/Cart.jsx" },
-      { path: "/history", component: "/src/pages/History.jsx" },
-    ],
-  },
+  base: "/food-delyvery-front",
 });
